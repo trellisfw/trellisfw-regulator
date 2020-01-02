b23bd94407a16c6f90eed76e87acd91d74bf3c32
Servio Palacios <servio@palacios.com>
2020-01-01 23:26:39 -0500

    [v0.1.2 - progressBar]

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,28 @@
+// "components/ProgressBar.jsx"
+import React                from "react";
+import { connect }          from "@cerebral/react";
+import { state } from "cerebral/tags";
+import { withStyles }       from "@material-ui/core/styles";
+import LinearProgress       from '@material-ui/core/LinearProgress';
+import { useStyles }        from "./config.js";
+
+class ProgressBar extends React.Component {
+  
+	render() {
+		const { classes } = this.props;
+		return (
+			 <div className={!this.props.open ? classes.hidden: classes.progressBar}>
+        <LinearProgress />
+      </div>
+		);
+	}
+}
+
+export default connect(
+	{
+		open:    state`ProgressBar.open`,
+    appName: state`ProgressBar.completed`
+	},
+	withStyles(useStyles, {withTheme: true})(ProgressBar)
+);
+
